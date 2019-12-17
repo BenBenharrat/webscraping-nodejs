06d4d4bb95496a9d3860dc3b7e8b561a96abf6ac
BenBenharrat <benjaminbenharrat@gmail.com>
2019-12-17 21:32:29 +0100

    add product name catcher feature

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const puppeteer = require('puppeteer');
 
+let productUrl = ''
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newpage();
+    await page.goto(productUrl);
+
+    let productData = await.page.evaluate(() => {
+        let productJson = {};
+        try { productJson.name = document.querySelector('h1.product-detail--title').innerText
+            
+        } catch (exception) {
+            
+        }
+    })
+})
